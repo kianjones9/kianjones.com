584c34f1de69bdcc23a7b53b5d365fbc63bfb076
Kian Jones <kian@kianjones.com>
2022-01-10 16:20:17 -0500

    github action for compiling post v1

--- .github/compile.py
+++ .github/compile.py
@@ -1,1 +1,40 @@
+import os
+import yaml
 
+file = open("base-post.html","r")
+base = file.read()
+file.close()
+
+def get_content(filename):
+
+    f = open(filename, "r")
+    text = f.read()
+    f.close()
+    
+    open_tag = "<article>"
+    start_index = text.find(open_tag)
+    close_tag = "</article>"
+    end_index = text.find(close_tag)
+
+    content =  text[start_index:end_index + len(close_tag)]
+    return content
+
+for post_dir in os.listdir("posts/"):
+    
+    f = open(post_dir + "post.yaml")
+    metadata = yaml.load(f)
+    f.close()
+
+    if metadata.processed == True:
+        continue
+
+    content = get_content(metadata.filename)
+    
+    tag = "<div class=\"content\">"
+    index = base.find(tag)
+    post = base[:index + len(tag)] + content + base[len(tag) + index:]
+
+
+    file = open("posts/" + metadata.filename,"w")
+    file.write(post)
+    file.close()
